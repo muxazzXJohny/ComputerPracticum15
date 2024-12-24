fc98eceb5b4152fbb536f7115d7c351859560da6
muxazzXJohny <maracoco334@gmail.com>
2024-12-24 09:03:21 +0200

    Add files via upload

---
+++ ConsoleApp15-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        static void Func()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{20}\t");
+            }
+        }
+    }
+}
