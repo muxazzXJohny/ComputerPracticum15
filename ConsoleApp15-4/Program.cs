69ef997f366bbb52068ab4b26d01424c6a71fad8
muxazzXJohny <maracoco334@gmail.com>
2024-12-24 09:27:39 +0200

    Add files via upload

---
+++ ConsoleApp15-4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        static void Func()
+        {
+            Random rud = new Random();
+            for (int x = 1; x <= 10; x++)
+            {
+                int number = rud.Next(-40, 40);
+                Console.WriteLine($"{x}-е число={number}");
+            }
+;
+        }
+    }
+}
