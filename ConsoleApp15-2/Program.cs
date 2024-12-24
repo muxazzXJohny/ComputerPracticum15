d8494fae96fb02364cf0b240582f8462df6337cb
muxazzXJohny <maracoco334@gmail.com>
2024-12-24 09:12:13 +0200

    Add files via upload

---
+++ ConsoleApp15-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        static void Func()
+        {
+            
+            for (int i = 2; i < 20; i++)
+            {
+                Console.WriteLine($"{i} штук = {i * 20.4} руб");
+            }
+;
+        }
+    }
+}
